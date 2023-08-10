cdd0eb78a2db99b9aa5e5ba028c8735155499f0c
VonjareeW <52430595+VonjareeW@users.noreply.github.com>
2023-08-10 11:20:21 -0400

    updated error found in code from console

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
   const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
   const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numericChars = '0123456789';
-  var SpecialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-  
+  const specialChars = '~!@#$%^&*()_+{}|:"<>?'
+
   let availableChars = '';
 
     if (includeLowercase) {
@@ -27,7 +27,7 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     password += availableChars[randomIndex];
   }
   if (password.length < 8 || password.length > 128) {
-    return "The length of the password must be within 8 to 128 characters.”;
+    return 'The length of the password must be within 8 to 128 characters.' ;
   }
 
   function getPasswordCriteria() {

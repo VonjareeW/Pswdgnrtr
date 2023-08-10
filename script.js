fab39bfe143300c617f99348d9e5171f46778668
VJ Wilson <52430595+VonjareeW@users.noreply.github.com>
2023-08-10 09:03:05 -0400

    Updated script by adding of statements regarding chosen characters to make password

--- script.js
+++ script.js
@@ -2,12 +2,39 @@
 
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSpecialChars) {
   let password = "";
-  const lowercaseChars = “abcdefghijklmnopqrstuvwxyz”;
-  const uppercaseChars = “ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
-  const numericChars = “0123456789”;
-  const specialChars = “!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz”;
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
+  const numericChars = "0123456789”;
+  const specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   
-  let availableChars = “”;
+  let availableChars = "”;
+
+    if (includeLowercase) {
+    availableChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    availableChars += uppercaseChars;
+  }
+  if (includeNumbers) {
+    availableChars += numericChars;
+  }
+  if (includeSpecialChars) {
+    availableChars += specialChars;
+  }
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex];
+  }
+  if (password.length < 8 || password.length > 128) {
+    return “The length of the password must be within  8 to 128 characters.”;
+  }
+  
+  return password;
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
